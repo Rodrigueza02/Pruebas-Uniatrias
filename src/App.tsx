cd972308c60623ab1195b11a09b43d2e255414ce
Rodrigueza02 <mariaj.rodrigueza@campusucc.edu.co>
2025-10-29 17:51:04 -0500

    correccion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
-function App(soool) {
+function App() {
   return (
     <BrowserRouter>
       <AppRoutes />
